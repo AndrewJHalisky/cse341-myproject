2bccbdbe954459b69dbde7991961eeef6c8eef14
AndrewJHalisky <ajhalisky@gmail.com>
2024-06-29 20:24:52 -0400

    Put .env in gitignore

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {res.send('Hello world!');});
 
+router.use('/', require('./swagger'));
+
 router.use('/groceries', require('./groceries'));
 
 module.exports = router;
